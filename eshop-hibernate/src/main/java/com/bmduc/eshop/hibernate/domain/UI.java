34d93db8f266d9ce9c9b96cb311e4a785145841a
Duc Bui <buimaiduc@gmail.com>
2015-10-23 18:15:36 +0700

    Update Background and UI models
    Signed-off-by: Duc Bui <buimaiduc@gmail.com>

--- eshop-hibernate/src/main/java/com/bmduc/eshop/hibernate/domain/UI.java
+++ eshop-hibernate/src/main/java/com/bmduc/eshop/hibernate/domain/UI.java
@@ -8,6 +8,13 @@
  */
 package com.bmduc.eshop.hibernate.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * ####################################################<br/>
  *   Copyright © Duc Bui 2015-2016<br/>
@@ -16,6 +23,50 @@ package com.bmduc.eshop.hibernate.domain;
  * Creation Time: 6:05:25 PM<br/>
  * @author DucBui<br/>
  */
-public class UI {
 
+@Entity
+@Table(name = "ES_UI")
+public class UI {
+	private long id;
+	private String name;
+	private String path;
+	private boolean active;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "UI_ID")
+	public long getId() {
+		return id;
+	}
+	
+	@Column(name = "UI_NAME")
+	public String getName() {
+		return name;
+	}
+	
+	@Column(name = "UI_PATH")
+	public String getPath() {
+		return path;
+	}
+	
+	@Column(name = "UI_ACTIVE")
+	public boolean isActive() {
+		return active;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	public void setActive(boolean active) {
+		this.active = active;
+	}
 }
