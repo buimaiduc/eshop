2e907ae8f8086a60470abfd878a2a57e7be6744c
Duc Bui <buimaiduc@gmail.com>
2015-10-26 23:03:39 +0700

    Commit Product Evaluation model

---
+++ eshop-hibernate/src/main/java/com/bmduc/eshop/hibernate/domain/ProductEvaluation.java
@@ -0,0 +1,21 @@
+/*
+ * ####################################################<br/>
+ *   Copyright © Duc Bui 2015-2016<br/>
+ * ####################################################<br/>
+ * Creation date: Oct 24, 2015<br/>
+ * Creation Time: 8:45:03 AM<br/>
+ * @author ducb<br/>
+ */
+package com.bmduc.eshop.hibernate.domain;
+
+/**
+ * ####################################################<br/>
+ *   Copyright © Duc Bui 2015-2016<br/>
+ * ####################################################<br/>
+ * Creation date: Oct 24, 2015<br/>
+ * Creation Time: 8:45:03 AM<br/>
+ * @author ducb<br/>
+ */
+public class ProductEvaluation {
+
+}
